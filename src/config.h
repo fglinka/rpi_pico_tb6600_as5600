a76c6acb17ce2d418dd8219214ce5baaeb79b7ce
Felix Glinka <devglinka@posteo.eu>
2025-05-24 21:24:30 +0200

    Start some c code

---
+++ src/config.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <stdint.h>
+
+enum ConnectionType {
+    COMMON_ANODE,
+    COMMON_CATHODE
+};
+
+struct StepperConfig {
+    // Whether the stepper is wired in common anode or cathode configuration
+    enum ConnectionType connection_type;
+    // The GPIO pin wired to the ENA+/- pin
+    const unsigned int pin_enable;
+    // The GPIO pin wired to the DIR+/- pin
+    const unsigned int pin_direction;
+    // The GPIO pin wired to the PUL+/- pin
+    const unsigned int pin_pulse;
+    // The step angle of the stepper (ignoring microstep mode) in Q16.16. Usually 360/200=1.8°.
+    const uint32_t step_angle;
+    // The amound of subdivision set for microstepping.
+    const unsigned int step_subdivisions;
+    // (Positive) velocity limit in Degrees Q16.16. Both for forward and reverse.
+    const uint32_t max_velocity;
+};
+
+const struct StepperConfig STEPPER_CONFIGS[] = {
+    {COMMON_CATHODE, 0, 1, 2, (uint32_t)(1.8 * (1 << 16)), 1, (uint32_t)(20 * (1 << 16))}
+};
+
+const unsigned int NUM_STEPPERS = 1;
